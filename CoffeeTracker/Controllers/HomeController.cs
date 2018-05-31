8cb1f573e1d4d87dbd3ff313cd5e604dc877955a
Jacob Morehouse <jacob.morehouse@gmail.com>
2018-05-30 21:19:07 -0700

    Got top X query working

--- CoffeeTracker/Controllers/HomeController.cs
+++ CoffeeTracker/Controllers/HomeController.cs
@@ -5,20 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using CoffeeTracker.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeTracker.Controllers
 {
     public class HomeController : Controller
     {
+        //private CoffeeTrackerContext coffeeContext;
+
+        private readonly CoffeeTrackerContext _context; //Instantiates the context
+        public HomeController(CoffeeTrackerContext context) //assigns the context to this controller.
+        {
+            this._context = context;
+        }
+
         public IActionResult Index()
         {
+
+            //var context = new CoffeeTrackerContext();
+            var topten = (from cof in _context.Coffee
+                            orderby cof.recorded descending
+                            select cof).Take(10);
+
+            ViewBag.topten = topten;
             return View();
         }
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
